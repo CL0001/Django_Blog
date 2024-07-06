0d06a5ce112ba056055921065df9206c4ba87e59
CL0001 <kov.adam09@gmail.com>
2024-07-06 11:27:58 +0200

    sort by category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Blog,Category
+
+def posts_by_category(request, category_id):
+    posts = Blog.objects.filter(category=category_id)
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        return redirect('home')
+    
+    context = {
+        'posts' : posts,
+        'category' : category,
+    }
+    return render(request, 'posts_by_category.html', context)
