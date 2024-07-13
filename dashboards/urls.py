85e3460e336161d1bab0b188b43589ffddd4ad01
CL0001 <kov.adam09@gmail.com>
2024-07-13 12:50:01 +0200

    user dashboard

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -5,13 +5,19 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     # Category curd
     path('categories/', views.categories, name='categories'),
-    path('add_category/', views.add_category, name='add_category'),
-    path('edit_category/<int:pk>/', views.edit_category, name='edit_category'),
-    path('delete_category/<int:pk>/', views.delete_category, name='delete_category'),
+    path('categories/add/', views.add_category, name='add_category'),
+    path('categories/edit/<int:pk>/', views.edit_category, name='edit_category'),
+    path('categories/delete/<int:pk>/', views.delete_category, name='delete_category'),
 
     # Blog curd
     path('posts/', views.posts, name='posts'),
-    path('post/add/', views.add_post, name='add_post'),
-    path('post/edit/<int:pk>/', views.edit_post, name='edit_post'),
-    path('post/delete/<int:pk>/', views.delete_post, name='delete_post'),
+    path('posts/add/', views.add_post, name='add_post'),
+    path('posts/edit/<int:pk>/', views.edit_post, name='edit_post'),
+    path('posts/delete/<int:pk>/', views.delete_post, name='delete_post'),
+
+    # Users
+    path('users/', views.users, name='users'),
+    path('users/add/', views.add_user, name='add_user'),
+    path('users/edit/<int:pk>', views.edit_user, name='edit_user'),
+    path('users/delete/<int:pk>', views.delete_user, name='delete_user'),
 ]
